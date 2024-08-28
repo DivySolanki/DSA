fc430de6f5049d126c87653da2b4bbee2f972636
Divy Solanki <divysolanki7@gmail.com>
2024-08-28 22:14:43 +0530

    Completed Word Ladder 2 and made changes to .gitignore

--- 10_Graph/2_sums.cpp
+++ 10_Graph/2_sums.cpp
@@ -24,7 +24,7 @@ If the queue is empty return 0 else the code in the queue will return the step r
 int wordLadderLength(string startWord, string targetWord, vector<string> &wordList) {
     queue<pair<string, int>> q;
     q.push({startWord, 1});
-    set<string> st(wordList.begin(), wordList.end());
+    unordered_set<string> st(wordList.begin(), wordList.end());
 
     while(!q.empty()) {
         string word = q.front().first;
@@ -52,6 +52,58 @@ int wordLadderLength(string startWord, string targetWord, vector<string> &wordLi
     return 0;
 }
 
+/*
+There is another variety of the above problem in which we have to return the vector of vectors.
+They contain all the shortest transformations from start to end word.
+*/
+vector<vector<string>> wordLadder2(string startWord, string targetWord, vector<string> &wordList) {
+    unordered_set<string> st(wordList.begin(), wordList.end());
+    queue<vector<string>> q;
+    q.push({startWord});
+    vector<string> usedOnLevel;
+    usedOnLevel.push_back(startWord);
+    int level = 0;
+    vector<vector<string>> ans;
+
+    while(!q.empty()) {
+        vector<string> vec = q.front();
+        q.pop();
+
+        if(vec.size() > level) {
+            level++;
+            for(auto it: usedOnLevel) {
+                st.erase(it);
+            }
+            usedOnLevel.clear();
+        }
+
+        string word = vec.back();
+
+        if(word == targetWord) {
+            if(ans.size() == 0) {
+                ans.push_back(vec);
+            }
+            else if(ans[0].size() == vec.size()) {
+                ans.push_back(vec);
+            }
+        }
+
+        for(int i=0; i<word.size(); i++) {
+            char original = word[i];
+            for(char ch='a'; ch<='z'; ch++) {
+                word[i] = ch;
+                if(st.find(word) != st.end()) {
+                    vec.push_back(word);
+                    q.push(vec);
+                    usedOnLevel.push_back(word);
+                    vec.pop_back();
+                }
+            }
+            word[i] = original;
+        }
+    }
+    return ans;
+}
 
 int main() {
     string startWord = "hit";
@@ -60,7 +112,14 @@ int main() {
         "hot", "dot", "dog", "lot", "log", "cog"
     };
 
-    cout << wordLadderLength(startWord, endWord, wordList);
+    vector<vector<string>> res =  wordLadder2(startWord, endWord, wordList);
+
+    for(auto f: res) {
+        for(auto s: f) {
+            cout << s << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
